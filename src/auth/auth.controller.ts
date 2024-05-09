62b86755a7518a94cbb8c4c45fb1239b37d1ffe2
Nico <nico_uicich@outlook.com>
2024-05-09 17:18:38 -0300

    change variable

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,80 +11,75 @@ import { AuthPayloadDto } from './dto/auth.dto';
 import { UserDto } from 'src/user/dto/user.dto';
 import { User } from 'src/user/entities/user.entity';
 import { UnauthorizedExceptionInterceptor } from 'libs/interceptors/invalid-credentials.interceptor';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {}
 
-    constructor(
-        private readonly authService: AuthService,
-    ) {}
+  @Post('login')
+  @UseGuards(LocalGuard)
+  @UseInterceptors(UnauthorizedExceptionInterceptor)
+  @ApiProperty({ type: AuthPayloadDto })
+  login(@Req() req: Request, @Res() res: Response) {
+    const token = this.authService.login(req.user as UserDto);
+    res.setHeader('Authorization', `Bearer ${token.access_token}`);
+    res.cookie('jwt', token.access_token, {
+      httpOnly: true,
+      // Deberías usar 'secure: true' en producción para enviar la cookie sólo sobre HTTPS
+      secure: false,
+      // sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
+    });
+    res.json({ user: req.user, token: token.access_token });
+  }
 
-    @Post('login')
-    @UseGuards(LocalGuard)
-    @UseInterceptors(UnauthorizedExceptionInterceptor)
-    @ApiProperty({ type: AuthPayloadDto })
-    login(@Req() req: Request, @Res() res: Response) {
-        const token = this.authService.login(req.user as UserDto);
-        res.setHeader('Authorization', `Bearer ${token.access_token}`);
-        res.cookie('jwt', token.access_token, {
-            httpOnly: true,
-            // Deberías usar 'secure: true' en producción para enviar la cookie sólo sobre HTTPS
-            secure: false,
-            // sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
-        });
-        res.json({ user: req.user, token: token.access_token });
-    }
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  @ApiProperty({ type: AuthPayloadDto })
+  logout(@Req() req: Request) {
+    // Perform logout logic here
+    return { msg: 'Logged out successfully' };
+  }
 
-    @Post('logout')
-    @UseGuards(JwtAuthGuard)
-    @ApiProperty({ type: AuthPayloadDto })
-    logout(@Req() req: Request) {
-        // Perform logout logic here
-        return { msg: 'Logged out successfully' };
-    }
+  @ApiCreatedResponse({ description: 'The user has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Bad request.' })
+  @Post('register')
+  @ApiOperation({ summary: 'Create a new user' })
+  @UseInterceptors(UsernameAlreadyExistsInterceptor)
+  @UseInterceptors(EmailAlreadyExistsInterceptor)
+  async createUser(@Body() createUserDto: CreateUserDto, @Req() req: Request, @Res() res: Response) {
+    const { token } = await this.authService.createUser(createUserDto, req);
 
-    @ApiCreatedResponse({ description: 'The user has been successfully created.' })
-    @ApiBadRequestResponse({ description: 'Bad request.' })
-    @Post('register')
-    @ApiOperation({ summary: 'Create a new user' })
-    @UseInterceptors(UsernameAlreadyExistsInterceptor)
-    @UseInterceptors(EmailAlreadyExistsInterceptor)
-    async createUser(@Body() createUserDto: CreateUserDto,
-        @Req() req: Request,
-        @Res() res: Response
-    ) {
-        const { token } = await this.authService.createUser(createUserDto, req);
+    res.setHeader('Authorization', `Bearer ${token}`);
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: false,
+      // sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
+    });
+    res.json({ user: req.user, token: token });
+  }
 
-        res.setHeader('Authorization', `Bearer ${token}`);
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: false,
-            // sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
-        });
-        res.json({ user: req.user, token: token });
-    }
+  @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
+  handleGoogleLogin() {}
 
-    @Get('google/login')
-    @UseGuards(GoogleAuthGuard)
-    handleGoogleLogin() {}
-
-    @Get('google/redirect')
-    @UseGuards(GoogleAuthGuard)
-    async googleLoginRedirect(
-        @Req() req: Request,
-        @Res() res: Response
-    ) {
-        const { token } = this.authService.createToken(req.user as User);
-        res.setHeader('Authorization', `Bearer ${token}`);
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: false,
-            // sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
-        });
-        // res.json({ user: req.user, token: token });
-        res.redirect(`http://localhost:4000/login?token=${token}`);
-    }
+  @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
+  async googleLoginRedirect(@Req() req: Request, @Res() res: Response) {
+    const { token } = this.authService.createToken(req.user as User);
+    res.setHeader('Authorization', `Bearer ${token}`);
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: false,
+      // sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 semana
+    });
+    // res.json({ user: req.user, token: token });
+    res.redirect(`${this.configService.get(`FRONT_URL`)}login?token=${token}`);
+  }
 }
